a344bb9bf22a37811db5bf7d2c470ebdc9e873ca
Jean <jeanvbonimenezes@gmail.com>
2021-11-18 20:24:51 -0400

    Implementando dto familia

---
+++ SeletorFamilias/SeletorFamilias.WepApp/DTOs/FamiliaDTO.cs
@@ -0,0 +1,24 @@
+﻿using SeletorFamilias.WepApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeletorFamilias.WepApp.DTOs
+{
+    public class FamiliaDTO
+    {
+        public string NomeResponsavel { get; set; }
+        public string CPFResponsavel { get; set; }
+        public string NomeConjuge { get; set; }
+        public string CPFConjuge { get; set; }
+
+        public FamiliaDTO(Familia familia)
+        {
+            NomeResponsavel = familia.Responsavel.Nome;
+            NomeConjuge = familia.Conjuge.Nome;
+            CPFResponsavel = familia.Responsavel.Cpf;
+            CPFConjuge = familia.Conjuge.Cpf;
+        }
+    }
+}
